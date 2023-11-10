9f16de72bacca5dfbb971392835c4accf81fabce
Adam Bolek <adama5.7241@gmail.com>
2023-11-09 22:25:53 -0500

    fixed runtime library in premake config

--- premake5.lua
+++ premake5.lua
@@ -22,3 +22,15 @@ project "yamlCpp"
     {
         "YAML_CPP_STATIC_DEFINE"
     }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
